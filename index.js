df11967bd784a6cd7cb228b4461798e33f8d0fb1
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-11-14 15:57:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
-
+const oldID = Math.floor(Math.random() * 100) + 1;
 const newID = (1000000000 + oldID)
 const ages = [26.4, 21, 49.5, 83, 99, 53.3, 74, 94.9, 40, 56.2];
 const currentAge = ages[Math.floor(Math.random() * ages.length)];

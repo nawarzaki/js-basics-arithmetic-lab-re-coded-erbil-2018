4ecd5088e2f699dea518d9597919eb7ba27cb7ff
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-11-14 15:11:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+const oldID = Math.floor(Math.random() * 100) + 1;
+const newID = (1000000000 + oldID)
